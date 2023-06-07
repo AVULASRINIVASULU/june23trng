205355498912abe13ec18750f890ad396917e562
AVULASRINIVASULU <131239839+AVULASRINIVASULU@users.noreply.github.com>
2023-06-07 09:05:19 +0530

    Add files via upload

---
+++ Palindrome1000.java
@@ -0,0 +1,17 @@
+public class Palindrome1000 {
+    public static void main(String[] args) {
+        int num;
+        for (int i = 1; i <= 1000; i++) {
+            num = i;
+            int rev = 0;
+            while (num != 0) {
+                int digit = num % 10;
+                rev = rev * 10 + digit;
+                num /= 10;
+            }
+            if (i == rev) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
